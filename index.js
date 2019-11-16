9e8ef35d15a227f30d55029fc615218ad2f0e77b
github username <github email address>
2019-11-16 15:48:31 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+const people = ["Hope", "Gene", "Raq"];
+
+function writeCards(people, event) {
+    let x = [];
+for (let i=0; i< people.length; i++ ) {
+x.push(`Thank you, ${people[i]}, for the wonderful ${event} gift!`);
+}
+return x;
+}
+
+function countDown() {
+let y = 10;
+while (y >= 0) {
+  console.log(y--);
+}
+}

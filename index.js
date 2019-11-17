9f68fa863d6c3e4ef56c59614b2a8a40dcfa236e
RubenV-Dev <r.will16@hotmail.com>
2019-11-17 13:01:47 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(arr, eventName) {
+    for (let i = 0; i < arr.length; i++) {
+        let currentName = arr[i];
+        arr.splice(i, 1, `Thank you, ${currentName}, for the wonderful ${eventName} gift!`);
+    }
+    return arr;
+}
+
+function countDown(num) {
+
+    while (num >= 0) {
+        console.log(num);
+        num--;
+    };
+}
+

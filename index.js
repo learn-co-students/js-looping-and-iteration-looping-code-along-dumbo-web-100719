e57f6ec44e7f6eba3b8b2f4a3a146e91050893bd
Jon Caneiro <joncaneiro@gmail.com>
2019-11-17 20:30:48 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solutions in this file
+
+const names = ['Lisa', 'Kaitlin', 'Jan']
+
+function writeCards(names) {
+
+    let newArray = []
+
+    for (let i = 0; i < names.length; i++) {
+
+    newArray.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+
+    }
+
+    return newArray
+}
+
+// let num = 10
+
+
+function countDown() {
+    for (let i = 10; i >= 0; i -= 1) {
+        console.log(i)
+    }
+}

31b5c8652a4705fab00488bd4e83c4347f2b8fbe
robin-raq <robin.raq@gmail.com>
2019-11-17 22:32:30 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(names, event){
+   const thanks = []
+    for (let i=0; i< names.length; i++){
+      thanks[i] = `Thank you, ${names[i]}, for the wonderful surprise gift!`;
+    }
+    return thanks;
+}
+
+function countDown(integer){
+    while (integer >=0 ){
+        console.log(integer--);
+        
+    }
+}

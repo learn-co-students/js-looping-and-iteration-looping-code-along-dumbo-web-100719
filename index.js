a0cdeacfc34163dc9b93e197acd405a0776026c3
Hamidou Diallo <Dio.hamidou@gmail.com>
2019-11-17 19:27:23 -0500

    Done

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+const writeCards = (arr,event) =>{
+  let message = []
+  for(let i=0; i< arr.length; i++){
+    message.push(`Thank you, ${arr[i]}, for the wonderful ${event} gift!`)
+  }
+  return message
+}
+
+const countDown = num =>{
+  while(num>=0){
+    console.log(num)
+    num -=1
+  }
+}
